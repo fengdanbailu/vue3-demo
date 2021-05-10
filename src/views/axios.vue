1239a17490d40c4f4a32c9aa0c2bda514dcdb57b
guoming chen <chenguoming2015@163.com>
2021-05-10 22:32:51 +0800

    修复yarn build的错误

--- src/views/axios.vue
+++ src/views/axios.vue
@@ -2,7 +2,7 @@
  * @Author: gm.chen
  * @Date: 2021-05-02 22:45:31
  * @LastEditors: gm.chen
- * @LastEditTime: 2021-05-09 07:09:55
+ * @LastEditTime: 2021-05-10 22:12:28
  * @Description: file content
  * @FilePath: /vue3-demo/src/views/axios.vue
 -->
@@ -12,7 +12,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import axios from '@/utils/axios'
+import axios from '@/utils/request'
 
 export default defineComponent({
   setup() {
